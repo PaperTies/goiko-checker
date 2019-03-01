ed229d1c304d92c8ee54667a65d527ec93f7c015
Mario Sánchez <msanchez@trovit.com>
2019-03-01 18:05:19 +0100

    Updates

---
+++ precache-manifest.cae4b3c36be417fa1d8ba32095d47108.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "e52d0ed7a00f19b80d6d",
+    "url": "/static/js/main.e52d0ed7.chunk.js"
+  },
+  {
+    "revision": "fc63ea5c7166dfccc892",
+    "url": "/static/js/1.fc63ea5c.chunk.js"
+  },
+  {
+    "revision": "e52d0ed7a00f19b80d6d",
+    "url": "/static/css/main.e9117778.chunk.css"
+  },
+  {
+    "revision": "0c684b9eceb363990e52e4c5c46b0ecd",
+    "url": "/index.html"
+  }
+];
